4df708ebe72be25be50327b3a39edf81d70e21ea
Ana Julia <anajureis2008@gmail.com>
2026-03-22 22:14:15 -0300

    Update Produto.cs

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -7,12 +7,49 @@ namespace MauiAPPMinhasCompras.Models
 {
     public class Produto
     {
+        private string _descricao;
+        private double _quantidade;
+        private double _preco;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
 
-        public double Total { get => Quantidade * Preco; }
+        public string Descricao
+        {
+            get => _descricao;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new Exception("Por favor, preencha a descrição");
+
+                _descricao = value;
+            }
+        }
+
+        public double Quantidade
+        {
+            get => _quantidade;
+            set
+            {
+                if (value <= 0)
+                    throw new Exception("Quantidade deve ser maior que 0");
+
+                _quantidade = value;
+            }
+        }
+
+        public double Preco
+        {
+            get => _preco;
+            set
+            {
+                if (value <= 0)
+                    throw new Exception("Preço deve ser maior que 0");
+
+                _preco = value;
+            }
+        }
+
+        public double Total => Quantidade * Preco;
     }
 }
