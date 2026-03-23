6e1926c22e1722302e77cf92912812eb1481b4b1
Ana Julia <anajureis2008@gmail.com>
2026-03-22 22:15:01 -0300

    Update ListaProduto.xaml.cs

--- Views/ListaProduto.xaml.cs
+++ Views/ListaProduto.xaml.cs
@@ -15,11 +15,19 @@ public partial class ListaProduto : ContentPage
 
     protected async override void OnAppearing()
     {
-        List<Produto> tmp = await App.Db.GetAll();
+        try
+        {
+            lista.Clear();
 
-        tmp.ForEach(i => lista.Add(i));
+            List<Produto> tmp = await App.Db.GetAll();
+
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlertAsync("Ops", ex.Message, "OK");
+        }
     }
-
     private void ToolbarItem_Clicked(object sender, EventArgs e)
     {
         try
@@ -33,21 +41,22 @@ public partial class ListaProduto : ContentPage
         }
     }
 
-    private void MenuItem_Clicked(object sender, EventArgs e)
-    {
-
-    }
-
     private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
     {
-        string q = e.NewTextValue;
+        try
+        {
+            string q = e.NewTextValue;
 
-        lista.Clear();
+            lista.Clear();
 
-        List<Produto> tmp = await App.Db.Search(q);
+            List<Produto> tmp = await App.Db.Search(q);
 
-        tmp.ForEach(i => lista.Add(i));
-    }
+            tmp.ForEach(i => lista.Add(i));
+        } catch
+        {
+
+        }
+     }
 
     private void ToolbarItem_Clicked_1(object sender, EventArgs e)
     {
@@ -57,4 +66,55 @@ public partial class ListaProduto : ContentPage
 
         DisplayAlertAsync("Total dos Produtos", msg, "OK");
     }
+
+
+
+   
+
+    private async void lst_produtos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        Produto p = e.CurrentSelection.FirstOrDefault() as Produto;
+
+        if (p == null)
+            return;
+
+        string action = await DisplayActionSheetAsync(
+            p.Descricao,
+            "Cancelar",
+            null,
+            "Editar",
+            "Excluir");
+
+        if (action == "Editar")
+        {
+            await Navigation.PushAsync(new Views.EditarProduto
+            {
+                BindingContext = p
+            });
+        }
+        else if (action == "Excluir")
+        {
+
+           bool confirm = await DisplayAlertAsync(
+                "Tem Certeza?",
+                $"Remover {p.Descricao}?",
+                "Sim",
+                "Não");
+
+            if (confirm)
+            {
+                await App.Db.Delete(p.Id);
+                var all = await App.Db.GetAll();
+                await DisplayAlertAsync("DEBUG", $"Quantidade: {all.Count}", "OK");
+                lista.Remove(p);
+            }
+     
+        }
+
+        ((CollectionView)sender).SelectedItem = null;
+    }
+
+  
+
+
 }
